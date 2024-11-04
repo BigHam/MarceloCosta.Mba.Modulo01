7102ff02978363b8d165fd99df885ea5e1aba73a
Marcelo Costa <mcosta.dev@gmail.com>
2024-11-04 20:35:11 -0300

    Inicialização da Solution Mc>Blog e os Projetos Associados

---
+++ src/Mc.Blog/Mc.Blog.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Mc.Blog.Web.Models
+{
+  public class ErrorViewModel
+  {
+    public string RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+  }
+}
